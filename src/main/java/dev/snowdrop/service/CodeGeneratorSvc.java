b285a114b42c3d6be373ceb59a2842e753d115ac
cmoulliard <cmoulliard@redhat.com>
2024-07-15 17:08:46 +0200

    Add method to generate the Task from yaml
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- src/main/java/dev/snowdrop/service/CodeGeneratorSvc.java
+++ src/main/java/dev/snowdrop/service/CodeGeneratorSvc.java
@@ -1,13 +1,26 @@
 package dev.snowdrop.service;
 
 import dev.snowdrop.factory.konflux.pipeline.Params;
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import io.fabric8.tekton.client.DefaultTektonClient;
+import io.fabric8.tekton.client.TektonClient;
+import io.fabric8.tekton.pipeline.v1.Task;
+import io.fabric8.tekton.pipeline.v1.TaskBuilder;
+import io.fabric8.tekton.pipeline.v1.TaskSpec;
+import io.fabric8.tekton.pipeline.v1.TaskSpecBuilder;
 import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
-import java.io.InputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CodeGeneratorSvc {
 
@@ -35,5 +48,17 @@ public class CodeGeneratorSvc {
       }
 
       System.out.println(javaCode.toString());
+   }
+
+   public static void generateTaskFromYAML(String resourcePath) {
+       try {
+          TektonClient tkn = new DefaultTektonClient();
+          TaskSpec task = tkn.v1().tasks().load(new File(resourcePath)).item().getSpec();
+
+          TaskSpec tsBuilder = new TaskSpecBuilder(task).build();
+          System.out.println("Task : " + task );
+       } catch (Exception e) {
+           throw new RuntimeException(e);
+       }
    }
 }
