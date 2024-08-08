f009184b49c56efed59368e87c895bc8fd3e131d
cmoulliard <cmoulliard@redhat.com>
2024-08-08 10:44:48 +0200

    Use switch everywhere
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- src/main/java/dev/snowdrop/PipeBuilderApp.java
+++ src/main/java/dev/snowdrop/PipeBuilderApp.java
@@ -12,8 +12,12 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
+import static dev.snowdrop.factory.Type.KONFLUX;
+import static dev.snowdrop.factory.Type.TEKTON;
 import static dev.snowdrop.factory.konflux.pipeline.Pipelines.*;
 import static dev.snowdrop.factory.tekton.pipeline.Pipelines.*;
+import static dev.snowdrop.model.Domain.BUILD;
+import static dev.snowdrop.model.Domain.BUILDPACK;
 import static dev.snowdrop.service.ApplicationComponentBuilder.createApplication;
 import static dev.snowdrop.service.ApplicationComponentBuilder.createComponent;
 
@@ -21,77 +25,64 @@ import static dev.snowdrop.service.ApplicationComponentBuilder.createComponent;
 @Command(name = "pipelinebuilder", mixinStandardHelpOptions = true, description = "Application generating Tekton Pipeline for Konflux")
 public class PipeBuilderApp implements Runnable {
 
-   private static final Logger logger = LoggerFactory.getLogger(PipeBuilderApp.class);
+    private static final Logger logger = LoggerFactory.getLogger(PipeBuilderApp.class);
 
-   @Option(names = {"-c", "--configuration-path"}, description = "The path of the configuration file", required = true)
-   String configuration;
+    @Option(names = {"-c", "--configuration-path"}, description = "The path of the configuration file", required = true)
+    String configuration;
 
-   @Option(names = {"-o", "--output-path"}, description = "The output path", required = true)
-   String outputPath;
+    @Option(names = {"-o", "--output-path"}, description = "The output path", required = true)
+    String outputPath;
 
-   public static void main(String[] args) {
-      int exitCode = new CommandLine(new PipeBuilderApp()).execute(args);
-      System.exit(exitCode);
-   }
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new PipeBuilderApp()).execute(args);
+        System.exit(exitCode);
+    }
 
-   @Override
-   public void run() {
-      logger.info("#### Configuration path: {}", configuration);
-      logger.debug("#### Output path: {}", outputPath);
+    @Override
+    public void run() {
+        logger.info("#### Configuration path: {}", configuration);
+        logger.debug("#### Output path: {}", outputPath);
 
-      // Parse and validate the configuration
-      Configurator cfg = ConfiguratorSvc.LoadConfiguration(configuration);
+        // Parse and validate the configuration
+        Configurator cfg = ConfiguratorSvc.LoadConfiguration(configuration);
 
-      if (cfg == null) {
-         logger.error("Configuration file cannot be empty !");
-         System.exit(1);
-      }
+        if (cfg == null) {
+            logger.error("Configuration file cannot be empty !");
+            System.exit(1);
+        }
 
-      if (cfg.getType() == null) {
-         logger.error("Type is missing from the configuration yaml file !");
-         System.exit(1);
-      } else {
-         logger.info("#### Type selected: {}", cfg.getType().toUpperCase());
-      }
+        if (cfg.getType() == null) {
+            logger.error("Type is missing from the configuration yaml file !");
+            System.exit(1);
+        } else {
+            logger.info("#### Type selected: {}", cfg.getType().toUpperCase());
+        }
 
-      if (cfg.getDomain() == null) {
-         cfg.setDomain(Domain.EXAMPLE.name());
-      }
-      logger.info("#### Pipeline domain selected: {}", cfg.getDomain());
+        if (cfg.getDomain() == null) {
+            cfg.setDomain(Domain.EXAMPLE.name());
+        }
+        logger.info("#### Pipeline domain selected: {}", cfg.getDomain());
 
-      String resourcesPath = outputPath + "/" + cfg.getDomain();
-      String providerType = cfg.getType().toUpperCase();
-      String domain = cfg.getDomain().toUpperCase();
+        String resourcesPath = outputPath + "/" + cfg.getDomain();
+        Type providerType = Type.valueOf(cfg.getType().toUpperCase());
+        Domain domain = Domain.valueOf(cfg.getDomain().toUpperCase());
 
-      // Type: Tekton
-      if (providerType == Type.TEKTON.name()) {
-         ConfiguratorSvc.writeYaml(createResource(cfg), resourcesPath);
-      }
-
-
-      // Type: Konflux
-      // Domain: build
-      // Resource generated: PipelineRun
-      // TODO: To be reviewed as not complete
-      if (providerType == Type.KONFLUX.name() && domain == Domain.BUILD.name()) {
-         // TODO: Enhance the factory to be able to generate the resource according to the resourceType: Pipeline, PipelineRun, Task
-         ConfiguratorSvc.writeYaml(createBuild(cfg), resourcesPath);
-      }
-
-      // Type: Konflux
-      // Domain: buildpack
-      // Resource generated: Pipeline
-      // TODO: To be reviewed as not complete
-      if (providerType == Type.KONFLUX.name() && domain == Domain.BUILDPACK.name()) {
-         // TODO: Enhance the factory to be able to generate the resource according to the resourceType: Pipeline, PipelineRun, Task
-         ConfiguratorSvc.writeYaml(createBuilder(cfg), resourcesPath);
-      }
-
-      // TODO: Add a boolean to enable to generate the following resources
-      if (providerType == Type.KONFLUX.name()) {
-         // Generate the Application, Component CR
-         ConfiguratorSvc.writeYaml(createApplication(cfg), resourcesPath);
-         ConfiguratorSvc.writeYaml(createComponent(cfg), resourcesPath);
-      }
-   }
+        switch (providerType) {
+            case TEKTON:
+                ConfiguratorSvc.writeYaml(createResource(cfg), resourcesPath);
+                break;
+            case KONFLUX:
+                // TODO: To be reviewed as not generated resources still include hard coded values, etc
+                switch (domain) {
+                    case BUILD:
+                        // Resource generated: PipelineRun
+                        ConfiguratorSvc.writeYaml(createBuild(cfg), resourcesPath);
+                        break;
+                    case BUILDPACK:
+                        // Resource generated: Pipeline
+                        ConfiguratorSvc.writeYaml(createBuilder(cfg), resourcesPath);
+                        break;
+                } // end of switch domain
+        } // end of switch providerType
+    } // end of run method
 }
