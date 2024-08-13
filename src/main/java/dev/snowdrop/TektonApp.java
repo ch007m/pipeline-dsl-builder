0b802618b7ffe7e2ec6505c1122350af211608c8
cmoulliard <cmoulliard@redhat.com>
2024-08-13 10:42:41 +0200

    Finishing to refactor the commands
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- src/main/java/dev/snowdrop/TektonApp.java
+++ src/main/java/dev/snowdrop/TektonApp.java
@@ -4,9 +4,7 @@ import dev.snowdrop.command.BuilderCommand;
 import dev.snowdrop.command.OCIBundleCommand;
 import dev.snowdrop.command.YamlToJavaCommand;
 import io.quarkus.picocli.runtime.annotations.TopCommand;
-import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
-import jakarta.inject.Inject;
 import picocli.CommandLine;
 
 @QuarkusMain
@@ -19,12 +17,9 @@ import picocli.CommandLine;
     },
     mixinStandardHelpOptions = true,
     description = "Tekton commands")
-public class TektonApp implements QuarkusApplication {
-    @Inject
-    CommandLine.IFactory factory;
-
-    @Override
-    public int run(String... args) throws Exception {
-        return new CommandLine(this, factory).execute(args);
+public class TektonApp {
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new TektonApp()).execute(args);
+        System.exit(exitCode);
     }
 }
--- src/main/java/dev/snowdrop/command/OCIBundleCommand.java
+++ src/main/java/dev/snowdrop/command/OCIBundleCommand.java
@@ -17,6 +17,7 @@ import picocli.CommandLine;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
@@ -25,25 +26,24 @@ import java.util.stream.Collectors;
 @CommandLine.Command(name = "extractBundle", description = "Fetch and extract the YAML resources from OCI bundle")
 public class OCIBundleCommand implements Runnable {
 
-    private static final String OUTPUT_OCI_PATH = "temp/oci";
     private static final String OUTPUT_TEKTON_PATH = "temp/oci/tekton";
-    private static final String INDEX_OCI_FILE_PATH = OUTPUT_OCI_PATH + "/index.json";
-    private static final String BLOBS_DIRECTORY = OUTPUT_OCI_PATH + "/blobs/sha256/";
 
     @CommandLine.Option(names = {"-b", "--bundle"}, description = "The OCI bundle URL", required = true)
     String configuration;
+
+    @CommandLine.Option(names = {"-p", "--path"}, description = "Path where files will be extracted", required = true)
+    String path;
 
     private static final Logger logger = LoggerFactory.getLogger(OCIBundleCommand.class);
 
     @Override
     public void run() {
         // Fetch the OCI bundle
-        grabOCIBundle(configuration, OUTPUT_OCI_PATH);
+        grabOCIBundle(configuration, path);
 
         // Search about the layer packaging the Tekton task
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            File indexJsonfile = new File(INDEX_OCI_FILE_PATH);
+            File indexJsonfile = new File(path + "/index.json");
             Index index = new ObjectMapper().readValue(indexJsonfile, Index.class);
 
             // Get the mediaType digest => json file containing the layers
@@ -56,7 +56,7 @@ public class OCIBundleCommand implements Runnable {
             logger.info("digest: " + digest);
             logger.info("sha256: " + sha256);
 
-            Manifest manifest = new ObjectMapper().readValue(new File(BLOBS_DIRECTORY + "/" + sha256), Manifest.class);
+            Manifest manifest = new ObjectMapper().readValue(new File(path + "/blobs/sha256" + "/" + sha256), Manifest.class);
             if (manifest != null) {
                 // Searching about the layer containing the json file of the Task
                 logger.info("Manifest found within blobs folder");
@@ -68,7 +68,7 @@ public class OCIBundleCommand implements Runnable {
                     taskLayers.forEach(layer -> {
                         String layerDigest = layer.getDigest();
                         logger.info("Found task layer: " + layerDigest + " for task: " + layer.getAnnotations().get("dev.tekton.image.name"));
-                        extractAndProcessBlob(new File(BLOBS_DIRECTORY + "/" + layerDigest.substring(7, layerDigest.length())));
+                        extractAndProcessBlob(Paths.get(path, "/tekton"),new File(path + "/blobs/sha256" + "/" + layerDigest.substring(7, layerDigest.length())));
                     });
                 }
             }
@@ -107,7 +107,7 @@ public class OCIBundleCommand implements Runnable {
         }
     }
 
-    private static void extractAndProcessBlob(File blobFile) {
+    private static void extractAndProcessBlob(Path outPutPath, File blobFile) {
         try (FileInputStream fis = new FileInputStream(blobFile);
              GzipCompressorInputStream gis = new GzipCompressorInputStream(fis);
              TarArchiveInputStream tis = new TarArchiveInputStream(gis)) {
@@ -127,10 +127,10 @@ public class OCIBundleCommand implements Runnable {
                     logger.info("Path to file extracted: " + outputFile.getAbsolutePath() + "\n");
 
                     // Create the Tekton folder to copy the YAML files
-                    Files.createDirectories(Paths.get(OUTPUT_TEKTON_PATH));
+                    Files.createDirectories(outPutPath);
 
                     String yaml = asYaml(Files.readString(outputFile.toPath()));
-                    String yamlFileName = OUTPUT_TEKTON_PATH + "/" + outputFile.getName().replaceFirst("[.][^.]+$", ".yaml");
+                    String yamlFileName = outPutPath + "/" + outputFile.getName().replaceFirst("[.][^.]+$", ".yaml");
                     logger.debug("yaml file name: " + yamlFileName);
                     Files.write(Paths.get(yamlFileName), yaml.getBytes());
                 }
