e7d99de675570fc7ceec8677092d340bc73da51f
cmoulliard <cmoulliard@redhat.com>
2024-08-20 06:56:25 +0200

    Add TODO to customize the Contructor of snakeyaml
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- src/test/java/dev/snowdrop/ConstructConfigurator.java
+++ src/test/java/dev/snowdrop/ConstructConfigurator.java
@@ -6,6 +6,7 @@ import org.yaml.snakeyaml.nodes.Node;
 
 public class ConstructConfigurator extends AbstractConstruct {
     public Object construct(Node node) {
+        // TODO How can we get from the node the object configurator which has been created from the YAML parsed
         Configurator cfg = new Configurator();
         // You can add custom initialization here
         return cfg;
