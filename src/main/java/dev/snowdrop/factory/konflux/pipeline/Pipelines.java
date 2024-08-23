5fd4efc95b0b0a0082e3bd080fcb691560aef1e8
cmoulliard <cmoulliard@redhat.com>
2024-08-23 16:01:49 +0200

    Add new configuration for konflux where task is defined as a bash script
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- src/main/java/dev/snowdrop/factory/konflux/pipeline/Pipelines.java
+++ src/main/java/dev/snowdrop/factory/konflux/pipeline/Pipelines.java
@@ -86,16 +86,16 @@ public class Pipelines implements JobProvider {
                 runAfter = "prefetch-dependencies";
             }
 
-            List<String> argList = new ArrayList<>();
+            List<String> args = new ArrayList<>();
             if (action.getArgs() != null && action.getArgs().size() > 0 ) {
-                argList = action.getArgs();
+                args = action.getArgs();
             }
             
             List<When> whenList = populateWhenList(action);
             List<TaskResult> taskResults = populateTaskResults(action.getResults());
 
             if (action.getScript() != null || action.getScriptFileUrl() != null) {
-                aTask = createTaskWithEmbeddedScript(action, runAfter, argList, whenList, jobWorkspacesMap, taskResults);
+                aTask = createTaskWithEmbeddedScript(action, runAfter, args, whenList, jobWorkspacesMap, taskResults);
                 tasks.add(aTask);
             }
 
