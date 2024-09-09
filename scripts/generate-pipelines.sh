35cc6961f2d3c204a2fb6473300f5dc593270525
cmoulliard <cmoulliard@redhat.com>
2024-09-09 15:33:44 +0200

    Adapt the code to test individual cfg files
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- scripts/generate-pipelines.sh
+++ scripts/generate-pipelines.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
+# Usage
+# ./scripts/generate-pipelines.sh
+# ./scripts/generate-pipelines.sh configurations/tekton/basic1-cfg.yaml
+
 CMD="java -jar target/quarkus-app/quarkus-run.jar builder"
 OUTPUT_PATH=out/flows
 
 rm -rf $OUTPUT_PATH
 cfg_files=(configurations/**/*)
 
-for file in "${cfg_files[@]}"; do
+# Check if a parameter (file name) is passed
+if [ $# -gt 0 ]; then
+  file="$1"
   $CMD -o $OUTPUT_PATH -c $file
-  [ $? -eq 0 ]  || exit 1
-done
+  exit
+else
+  for file in "${cfg_files[@]}"; do
+    $CMD -o $OUTPUT_PATH -c $file
+    [ $? -eq 0 ]  || exit 1
+  done
+fi
